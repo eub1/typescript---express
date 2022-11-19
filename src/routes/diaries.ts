9bcd9ff26d719dfbfd19f4d0a46e54013de09cc8
eub1 <ebonforte@gmail.com>
2022-11-19 15:29:35 -0300

    creo ruta get ./:id, y post

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -7,8 +7,17 @@ router.get('/', (_req, res) => {
   res.send(diaryServices.getEntrieswithoutSensitiveInfo())
 })
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!')
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body
+  const newDiaryEntry = diaryServices.addEntry(date, weather, visibility, comment)
+  res.json(newDiaryEntry)
 })
 
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(+req.params.id) // + unary operator, to convert string to number. lo que viene por req.params siempre es str
+  // res.send(diary?.weather)
+  return diary !== undefined
+    ? res.send(diary)
+    : res.sendStatus(404)
+})
 export default router
