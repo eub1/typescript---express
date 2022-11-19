a819fccd345c1f5c4ad4bd360c55633904a6bc10
eub1 <ebonforte@gmail.com>
2022-11-19 18:14:34 -0300

    crea enums.ts

---
+++ src/enums.ts
@@ -0,0 +1,17 @@
+// export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+// export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Windy = 'windy',
+  Stormy = 'stormy'
+}
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor'
+}
